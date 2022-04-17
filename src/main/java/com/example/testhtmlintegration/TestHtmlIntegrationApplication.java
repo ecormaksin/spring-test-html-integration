818e9b97e4e49bb194c007bf7ef5d675e75764fb
Makoto Oda <truth_jp_4133@yahoo.co.jp>
2022-04-17 14:37:14 +0900

    initial commit

---
+++ src/main/java/com/example/testhtmlintegration/TestHtmlIntegrationApplication.java
@@ -0,0 +1,13 @@
+package com.example.testhtmlintegration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestHtmlIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestHtmlIntegrationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/testhtmlintegration/TestHtmlIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testhtmlintegration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestHtmlIntegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
